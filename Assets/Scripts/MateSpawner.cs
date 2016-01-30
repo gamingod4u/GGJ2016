35720c8c45b93849424dfb83b7e0a1ade861f17f
Bill Jones <unrealguard57@hotmail.com>
2016-01-30 13:28:34 -0500

    Looking into spawning mate

---
+++ Assets/Scripts/MateSpawner.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class MateSpawner : MonoBehaviour
+{
+    private BackgroundScrolling _background;
+    private float _nextSpawn;
+
+    public GameObject MatePrefab;
+    public float SpawnInterval = 8.0f;
+    public float SpawnIntervalRange = 3.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        _background = GameObject.FindGameObjectWithTag("World")
+            .GetComponent<BackgroundScrolling>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_nextSpawn == default(float))
+        {
+            var timeTillNextSpawn = SpawnInterval + Random.Range(0, SpawnIntervalRange);
+            _nextSpawn = Time.time + timeTillNextSpawn;
+        }
+
+        if(_nextSpawn < Time.time)
+        {
+            SpawnMate();
+        }
+    }
+
+    void SpawnMate()
+    {
+        Debug.Log("Spawning Mate");
+        var mate = GameObject.Instantiate(MatePrefab, new Vector3(
+            transform.position.x, 
+            transform.position.y, 
+            transform.position.z), Quaternion.identity);
+    }
+}
