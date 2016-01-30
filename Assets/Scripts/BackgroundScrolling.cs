d98d40c53f502b6f4752a728fb01f357ddf91eba
gamingod4u <mcman407@gmail.com>
2016-01-30 10:18:58 -0500

    added player and scrolling background

---
+++ Assets/Scripts/BackgroundScrolling.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class BackgroundScrolling : MonoBehaviour
+{
+    public GameObject background;
+
+    public GameObject[] backgrounds;
+
+    public float speed;
+    void Start()
+    {
+
+    }
+
+
+
+    void Update()
+    {
+        speed = Input.GetAxis("Horizontal") * 5;
+        for (int i = 0; i < 2; i++)
+        {
+            backgrounds[i].transform.position += new Vector3(speed * Time.deltaTime, 0);
+
+
+            if (speed > .1 && backgrounds[i].transform.position.x > 20)
+            {
+                backgrounds[i].transform.position = new Vector3(backgrounds[i].transform.position.x - 40, 0);
+            }
+            else if (speed < -.1 && backgrounds[i].transform.position.x < -20)
+            {
+                backgrounds[i].transform.position = new Vector3(backgrounds[i].transform.position.x + 40, 0);
+            }
+        }
+    }
+}
